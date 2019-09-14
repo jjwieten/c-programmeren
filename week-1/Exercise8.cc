841e6b126bffe719aab1e636da5135ba0d4c4adf
Rutger Lemein <47449672+rjalemein@users.noreply.github.com>
2019-09-14 14:40:50 +0200

    Add files via upload

---
+++ week-1/Exercise8.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    size_t value = stoul(argv[1], 0, 16);	// initialize hexadecimal value
+    size_t nibble = stoul(argv[2]);		// nibble to replace
+    size_t replacement = stoul(argv[3]) % 16;	// new nibble (= 0 .. 15)
+
+    value &= ~(0xF << (nibble * 4));		// bit-mask to clear nibble from value
+    value |= replacement << (nibble * 4);	// insert new nibble in cleared position
+
+    cout << hex << value << '\n';		// show the new value
+}
